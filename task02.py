542f79e928973f325c6d8ece84ccaef0efbe1680
ubparviz <ubparviz@gmail.com>
2025-05-28 12:18:50 +0500

    Done

--- task02.py
+++ task02.py
@@ -5,7 +5,7 @@ sharif = input("Sharifingizni kiritng: ")
 raqam = random.randint(1, 100)
 
 print(f"""Sizga quyidaga "user name"larni taklif qilaman:   
-        \n{ism}.{sharif}
-        \n{ism}\\_{sharif}
-        \n{ism[0]}{sharif}{raqam}
-        \n{ism}{sharif[0]}"""  )
+        {ism}.{sharif}
+        {ism}\\_{sharif}
+        {ism[0]}{sharif}{raqam}
+        {ism}{sharif[0]}"""  )
